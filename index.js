fa3d1a98ab0c66fb6d2bf80f0f51bd505a7fbddc
Johnny Wong <johnny.wong2324@gmail.com>
2022-05-18 20:57:16 -0400

    init project

---
+++ index.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const port = process.env.PORT || 8000;
+
+// put your routes here
+// routes should begin with /api/
+// since the front end proxies requests to any url beginning with /api/
+// from port 3000 to port 8000 in development
+// example route:
+// app.post("/api/auth/register", (req, res) => {....})
+//
+// and in the front end:
+// axios.post("/api/auth/register", {formData}).then(result=>{....}).catch(err=>{....})
+
+// static file declaration
+if (process.env.NODE_ENV === 'production') {
+  // production mode
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// be sure to set your MONGO_URI in a .env file in both the root folder of your project
+// and in the config variables section on Heroku, in the settings page for your app
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is up on port ${port}!`);
+    });
+  });
