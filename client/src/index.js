217eb84ef678a03a5b10ccd4a9b407fc8ca5f709
Johnny Wong <johnny.wong2324@gmail.com>
2022-05-19 11:12:57 -0400

    react 18 stuff, use yarn

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ChakraProvider>
 );
