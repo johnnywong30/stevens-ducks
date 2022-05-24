80b13ac822f5c1ec0f947023606352eaeb50656a
Ryan Chin <ryanchin26@gmail.com>
2022-05-24 19:29:35 -0400

    Contact page needs CSS
    Contact info is all in, just reorder and style and we are good

--- client/src/routes/pages/contact/ContactCard.js
+++ client/src/routes/pages/contact/ContactCard.js
@@ -3,35 +3,21 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { Box, Heading, Badge, Text, Stack, HStack, VStack, Image } from '@chakra-ui/react'
 import Notion from "services/notion/";
+import '../../../css/cards.css'
 
 function Feature({ title, name, desc, year, img, major, ...rest }) {
     return (
-      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+      <Box class="card card-custom">
         <Image src={img} alt={name} fallbackSrc='https://via.placeholder.com/150'/>
-        <Box p='6'>
-            <Box display='flex' alignItems='baseline'>
+        <Box>
+            <Box>
                 <Badge borderRadius='full' px='2' colorScheme='teal'>
                     {year}
                 </Badge>
-                <Box
-                    color='gray.500'
-                    fontWeight='semibold'
-                    letterSpacing='wide'
-                    fontSize='xs'
-                    textTransform='uppercase'
-                    ml='2'
-                >
-                    Some Other Info...
+                <Box>
                 </Box>
-                <Box
-                    mt='1'
-                    fontWeight='semibold'
-                    as='h4'
-                    lineHeight='tight'
-                    noOfLines={1}
-                >
-                    {t}
-                    <Text mt={4}>{desc}</Text>
+                <Box>
+                    {title}
                 </Box>
                 <Box>
                     {desc}
@@ -62,12 +48,11 @@ function StackEx() {
                 return true;
 
             }).map((person) => 
-                      <Feature>
-                          name = {person.personName}
-                          title = {person.personRole}
-                          year = {person.personYear}
-                          major = {person.personMajor}
-                          desc = {person.personDesc}
+                      <Feature  name = {person.personName}
+                                title = {person.personRole}
+                                year = {person.personYear}
+                                major = {person.personMajor}
+                                desc = {person.personDesc}>
                       </Feature>
                   )} 
       </>
